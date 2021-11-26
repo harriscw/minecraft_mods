d142fc86026abac3b1fb72a74ef2b637637f58e1
harriscw <harriscw@ucla.edu>
2021-11-26 09:17:43 -0800

    blockstack

--- blockStack_.py
+++ blockStack_.py
@@ -1,8 +1,12 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-x=4
-y=64
-z=9
+position=mc.player.getTilePos()
+
+x=position.x+1
+y=position.y
+z=position.z
 blockType=103
-mc.setBlock(x,y+1,z,blockType)
+
+for i in range(6):
+    mc.setBlock(x,y+i,z,blockType)
